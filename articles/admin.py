db2504da2b72366d575c074ddf0fb7c8c88f743e
Bambotims <timilehinadebambo@gmail.com>
2023-12-09 21:29:31 +0100

    comment capabilities

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comments
+
+
+class CommentInline(admin.TabularInline):
+    model = Comments
+    extra = 0
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+
 
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comments)
